39a48f047ce80453b2a0c70120a6d80f0fda8b29
yadud1001 <ann1001tampus@gmail.com>
2025-02-24 17:55:47 +0800

    set ejs

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.send('index.html', {})
